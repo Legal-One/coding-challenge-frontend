d83683ef80bcd7f056b82171015e98d1f16d309a
Gregfanyan <grigorfanyan@gmail.com>
2021-02-14 00:50:13 +0100

    added resolution reducer

---
+++ client/src/redux/reducers/ResolutionReducer.tsx
@@ -0,0 +1,45 @@
+import {
+  FETCH_RESOLUTION_REQUEST,
+  FETCH_RESOLUTION_FAILURE,
+  FETCH_RESOLUTION_SUCCESS,
+  ResolutionActions,
+  ResolutionState,
+} from '../../Types/ResolutionType'
+
+const initialState: ResolutionState = {
+  loading: false,
+  resolution: [],
+  error: '',
+}
+
+const ResolutionReducers = (
+  state = initialState,
+  action: ResolutionActions
+) => {
+  switch (action.type) {
+  case FETCH_RESOLUTION_REQUEST:
+    return {
+      ...state,
+      loading: true,
+    }
+  case FETCH_RESOLUTION_SUCCESS:
+    return {
+      ...state,
+      loading: false,
+      resolution: action.payload,
+      error: '',
+    }
+  case FETCH_RESOLUTION_FAILURE:
+    return {
+      ...state,
+      loading: false,
+      resolution: [],
+      error: action.payload,
+    }
+
+  default:
+    return state
+  }
+}
+
+export default ResolutionReducers
