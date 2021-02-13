874dc4bc0607b4fb03a65e65ef639943bfbf28b0
Gregfanyan <grigorfanyan@gmail.com>
2021-02-13 17:10:14 +0100

    added agent hooks

---
+++ client/src/Hooks/useAgents.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { fetchAgents } from '../redux/actions/AgentActions'
+import { Agent } from '../Types/AgentType'
+import { AppState } from '../Types/'
+
+export const useAgents = () => {
+  const [agentData, setAgentData] = useState<Agent[]>([])
+  const agents = useSelector((state: AppState) => state.agents.agents)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchAgents())
+  }, [dispatch])
+
+  useEffect(() => {
+    setAgentData(agents)
+  }, [agents])
+
+  return [agentData]
+}
