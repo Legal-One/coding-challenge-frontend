64ce958a66d7e5354ec2fe1f4610558bf3f800d2
Gregfanyan <grigorfanyan@gmail.com>
2021-02-13 15:56:29 +0100

    added react-typescript boilerplate

---
+++ client/src/redux/actions/ui.ts
@@ -0,0 +1,10 @@
+import { TOGGLE_DIALOG, ToggleDialogAction, DialogType } from '../../types'
+
+export function toggleDialog(dialog: DialogType): ToggleDialogAction {
+  return {
+    type: TOGGLE_DIALOG,
+    payload: {
+      dialog,
+    },
+  }
+}
---
+++ client/src/redux/sagas/index.ts
@@ -0,0 +1,8 @@
+import { all } from 'redux-saga/effects'
+
+import productSagas from './product'
+import uiSagas from './ui'
+
+export default function* rootSaga() {
+  yield all([...productSagas, ...uiSagas])
+}
