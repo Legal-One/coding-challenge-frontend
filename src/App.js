149250c0ec1c84b1d8ef362a33406928b6384c34
cleandevcode <freelance1331@gmail.com>
2021-02-17 19:40:55 +0300

    front1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
+import FusionCharts from "fusioncharts";
+import charts from "fusioncharts/fusioncharts.charts";
+import ReactFusioncharts from "react-fusioncharts";
+
+charts(FusionCharts);
 
 class App extends Component {
   constructor(props) {
@@ -7,18 +12,56 @@ class App extends Component {
     this.state = {
       name: "",
       greeting: "",
-      logs: []
+      logs: [],
+      dataSource: {
+        chart: {
+          caption: "Top 5 countries with Global Oil Reserves",
+          subcaption: "MMbbl= One Million barrels",
+          enablesmartlabels: "0",
+          startingAngle: "0",
+          showPercentValues: "1",
+          decimals: "1",
+          seriesNameInToolTip: "0",
+          useDataPlotColorForLabels: "1",
+          theme: "fusion"
+        },
+        data: [
+          {
+            label: "+49151484522",
+            value: "3",
+            displayValue: "Lowest sale.{br}$920K",
+            //Using custom tooltip
+            tooltext: "Lowest sale for last week. Indiana $dataValue"
+          },
+          {
+            label: "+49158544147",
+            value: "2"
+          },
+          {
+            label: "+49151783331",
+            value: "1"
+          },
+          {
+            label: "+49151514231",
+            value: "1"
+          },
+          {
+            label: "+49221514231",
+            value: "2"
+          }
+        ]
+      }
     };
     this.handleCall = this.handleCall.bind(this);
     this.handleAgent = this.handleAgent.bind(this);
   }
 
   componentDidMount() {
-    fetch("/api")
-      .then(res => res.json())
-      .then(res => {
-        this.setState({ logs: res });
-      });
+    // fetch("/api")
+    //   .then(res => res.json())
+    //   .then(res => {
+    //     this.setState({ logs: res });
+    //   });
   }
 
   handleChange(event) {
@@ -46,12 +89,16 @@ class App extends Component {
   }
 
   render() {
+    const { dataSource } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <h3>Front-end Test</h3>
-          <button onClick={this.handleCall}>Test</button>
-        </header>
+        <ReactFusioncharts
+          type="pie3d"
+          width={800}
+          height={600}
+          dataFormat="JSON"
+          dataSource={dataSource}
+        />
       </div>
     );
   }
