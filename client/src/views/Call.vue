b1963d1b460380c35c0ea7b21db5a59836557c0c
Onikoko Abdullahi <onikokoabdullahi@gmail.com>
2021-02-20 02:19:22 +0800

    Implemented call details page

--- client/src/views/Call.vue
+++ client/src/views/Call.vue
@@ -1,10 +1,50 @@
 <template>
-  <div class="call"></div>
+  <div class="call">
+    <table id="table">
+      <thead>
+        <tr>
+          <th>Agent Name</th>
+          <th>Call date and time</th>
+          <th>Resolution</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in callHistory" :key="index">
+          <td>{{ item.agentName }}</td>
+          <td>{{ formatDate(item.dateTime) }}</td>
+          <td>{{ item.resolution }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
-  name: 'Call'
+  name: 'Call',
+  data () {
+    return {}
+  },
+  mounted () {
+    this.getCallHistory()
+    // console.log(this.$route.params.number)
+  },
+  computed: {
+    ...mapGetters(['callHistory'])
+  },
+  methods: {
+    ...mapActions(['fetchCallDetails']),
+    async getCallHistory () {
+      await this.fetchCallDetails(this.$route.params.number)
+    },
+
+    formatDate (date) {
+      var d = new Date(date)
+      return `${d.getDate()}/${d.getMonth()}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`
+    }
+  }
 }
 </script>
 
