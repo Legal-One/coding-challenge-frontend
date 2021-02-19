ab4c50141ed1a25143764c59c5441fbd90878617
Onikoko Abdullahi <onikokoabdullahi@gmail.com>
2021-02-20 00:29:35 +0100

    Refactored store actions

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -7,50 +7,38 @@ const BASE_URL = 'http://localhost:3000'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    agents: [],
-    call: [],
-    agent: []
-  },
+  state: {},
 
-  getters: {
-    allAgents: state => state.agents,
-    agent: state => state.agent,
-    callHistory: state => state.call
-  },
+  getters: {},
 
-  mutations: {
-    SET_AGENTS (state, agents) {
-      state.agents = agents
-    },
-    SET_AGENT (state, agent) {
-      state.agent = agent
-    },
-    SET_CALL_DETAILS (state, call) {
-      state.call = call
-    }
-  },
+  mutations: {},
 
   actions: {
-    fetchAgents ({ commit }) {
-      axios.get(`${BASE_URL}/agents`).then(response => {
-        // console.log(response)
-        commit('SET_AGENTS', response.data.data)
-      })
+    async fetchAgents ({ commit }) {
+      try {
+        const response = await axios.get(`${BASE_URL}/agents`)
+        return response.data.data
+      } catch (err) {
+        console.log(err)
+      }
     },
 
-    fetchAgent ({ commit }, id) {
-      axios.get(`${BASE_URL}/agent/${id}`).then(response => {
-        // console.log(response.data.data)
-        commit('SET_AGENT', response.data.data)
-      })
+    async fetchAgent ({ commit }, id) {
+      try {
+        const response = await axios.get(`${BASE_URL}/agent/${id}`)
+        return response.data.data
+      } catch (err) {
+        console.log(err)
+      }
     },
 
-    fetchCallDetails ({ commit }, number) {
-      axios.get(`${BASE_URL}/call/${number}`).then(response => {
-        // console.log(response.data.data)
-        commit('SET_CALL_DETAILS', response.data.data)
-      })
+    async fetchCallDetails ({ commit }, number) {
+      try {
+        const response = await axios.get(`${BASE_URL}/call/${number}`)
+        return response.data.data
+      } catch (err) {
+        console.log(err)
+      }
     }
   },
   modules: {}
