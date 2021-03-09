a7a51e59d48240f6cb1cec56690587c2ff038829
emredalkiran <emre59dalkiran@gmail.com>
2021-03-09 15:52:22 +0300

    Show loading message while waiting for data fetch

--- client/src/components/Home/index.tsx
+++ client/src/components/Home/index.tsx
@@ -34,43 +34,51 @@ function Home() {
       <h1 className="is-size-2 has-text-weight-bold pt-6 pb-6 has-text-centered">
         Call Details at a Glance
       </h1>
-      <div className="container is-flex is-justify-content-space-between">
-        <div className="card">
-          <span>Total Number of Calls</span>
-          <div className="is-size-2 has-text-weight-bold">{callStats.total.calls}</div>
-        </div>
+      {loadStatus === LoadStatus.LOADED ? (
+        <>
+          <div className="container is-flex is-justify-content-space-between">
+            <div className="card">
+              <span>Total Number of Calls</span>
+              <div className="is-size-2 has-text-weight-bold">{callStats.total.calls}</div>
+            </div>
 
-        <div className="card">
-          <span>Average Call Time</span>
-          <div className="is-size-2 has-text-weight-bold">
-            {callStats.total.calls > 0 &&
-              (callStats.total.duration / callStats.total.calls).toFixed(2)}
-            secs
+            <div className="card">
+              <span>Average Call Time</span>
+              <div className="is-size-2 has-text-weight-bold">
+                {callStats.total.calls > 0 &&
+                  (callStats.total.duration / callStats.total.calls).toFixed(2)}
+                secs
+              </div>
+            </div>
+            <div className="card">
+              <span>Unique Numbers Called</span>
+              <div className="is-size-2 has-text-weight-bold">
+                {Object.keys(callStats).length - 1}
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="card">
-          <span>Unique Numbers Called</span>
-          <div className="is-size-2 has-text-weight-bold">{Object.keys(callStats).length - 1}</div>
-        </div>
-      </div>
-      <div className="is-flex is-justify-content-center">
-        <ResolutionGraph />
-        <LineGraph />
-      </div>
-      <Table>
-        <TableHeader headers={tableHeaders} />
-        {Object.keys(callStats).map((key, index) => {
-          return (
-            key !== 'total' && (
-              <TableRow
-                key={key}
-                {...callStats[key]}
-                isLastRow={index === Object.keys(callStats).length - 1}
-              />
-            )
-          )
-        })}
-      </Table>
+          <div className="is-flex is-justify-content-center">
+            <ResolutionGraph />
+            <LineGraph />
+          </div>
+          <Table>
+            <TableHeader headers={tableHeaders} />
+            {Object.keys(callStats).map((key, index) => {
+              return (
+                key !== 'total' && (
+                  <TableRow
+                    key={key}
+                    {...callStats[key]}
+                    isLastRow={index === Object.keys(callStats).length - 1}
+                  />
+                )
+              )
+            })}
+          </Table>
+        </>
+      ) : (
+        <div className="is-size-3 has-text-weight-bold">Loading...</div>
+      )}
     </div>
   )
 }
