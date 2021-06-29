87257ad4e9f0fbe460c95f0d79c187fbbc54de35
Souravdey777 <piyush.kolkata@gmail.com>
2021-06-29 17:48:20 +0530

    🐛 dashboard api updated

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -5,18 +5,38 @@ const resolutions = require('../../json-data/resolution.json')
 
 // combining logs and resolutions and agents
 const logsWithResolutionsAndAgents = logs.map((log) => ({
-  ...log,
   ...resolutions.find((resolution) => resolution.identifier === log.identifier),
-  ...agents.find((agent) => agent.identifier === log.agentIdentifier)
+  ...agents.find((agent) => agent.identifier === log.agentIdentifier),
+  ...log // log is spreaded after the rest because otherwise the log.identifier value will get effected by agents.identifier
 }))
 
 // get Dashboard Data
 async function getDashboardData(req, res) {
   try {
+    // array of all the distinct phone numbers
+    const uniqueNumbers = logsWithResolutionsAndAgents
+      .map((eachData) => eachData.number)
+      .filter((value, index, self) => self.indexOf(value) === index)
+
+    const finalData = uniqueNumbers.map((number) => {
+      // logDataForNumber is the logs With Resolutions And Agents data combined for each phone number
+      let logDataForNumber = logsWithResolutionsAndAgents.filter(
+        (log) => log.number === number
+      )
+
+      // total calls made to a specific number
+      let callCount = logDataForNumber.length
+
+      // last call Record is the last value in the array logDataForNumber
+      let lastCallRecord = logDataForNumber[callCount - 1]
+
+      return { ...lastCallRecord, callCount } // last call record and call count
+    })
+
     return res.json({
       status: 'success',
       message: 'Dashboard data',
-      data: logsWithResolutionsAndAgents
+      data: finalData
     })
   } catch (err) {
     return res.json({ status: 'error', message: err })
