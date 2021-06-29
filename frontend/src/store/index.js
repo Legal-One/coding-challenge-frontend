e55bc510e89f30f074006ed76a740d26c50f9c70
Souravdey777 <piyush.kolkata@gmail.com>
2021-06-30 00:17:01 +0530

    redux store

---
+++ frontend/src/store/index.js
@@ -0,0 +1,29 @@
+import { applyMiddleware, createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import thunk from 'redux-thunk'
+import rootReducer from '../reducer'
+
+const persistConfig = {
+  key: 'root',
+  storage: storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+const endpoint = 'http://localhost:8000'
+let store = createStore(
+  persistedReducer,
+  applyMiddleware(thunk.withExtraArgument(endpoint))
+)
+
+store.subscribe(() => {
+  console.log(store.getState())
+})
+
+let persistor = persistStore(store)
+
+const getStore = () => {
+  return { store, persistor }
+}
+
+export default getStore
