762927dafacc24953a9d829dfad85113d439ee93
pratiksha-chaudhary <pratiksha.chaudhary89@gmail.com>
2021-07-23 14:56:24 +0200

    cleanup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -10,7 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(cors());
 app.use('/', routes);
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
