03c738a056503c1adfc887e49642e3623a498549
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2021-12-23 17:13:54 +0300

    add import resolver

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -14,4 +14,18 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
+  settings: {
+    "import/resolver": {
+      node: {
+        "extensions": [".js", ".jsx", ".ts", ".tsx"]
+      },
+      alias: {
+        "map" : [
+          ["@components","./src/components/*"]
+          ["@","./src/*"]
+        ],
+        "extensions": [".vue"]
+      },
+    }
+  },
 };
