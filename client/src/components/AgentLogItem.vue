785d427f99979c680b53af0f0a5a0ccb5674fae2
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2021-12-26 21:24:00 +0300

    create agent log item

---
+++ client/src/components/AgentLogItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="agent-log-item">
+    <div class="agent-log-item__number">
+      {{ phoneLog.number }}
+    </div>
+    <div class="agent-log-item__date">
+      {{ phoneLog.dateTime }}
+    </div>
+    <div class="agent-log-item__resolutions">
+      {{ phoneLog.resolution }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AgentLogItem',
+  props: {
+    phoneLog: Object,
+  },
+};
+</script>
+
+<style scoped>
+  .agent-log-item {
+    display: grid;
+    width: 100%;
+    height: 70px;
+    grid-template-columns: 3fr 1fr 3fr;
+  }
+</style>
