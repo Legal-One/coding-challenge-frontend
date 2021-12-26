f29bddede48828fb2af8d1e7db667888259347dc
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2021-12-26 15:22:09 +0300

    update phone-logs.spec

--- handlers/test/phone-logs.spec.js
+++ handlers/test/phone-logs.spec.js
@@ -6,27 +6,42 @@ const {
 const mockPhoneLogs = require('./phone-logs-mock.json');
 
 const newPhoneLogDummyData = {
-    number: "+49151484522",
+    number: '+49151484522',
     logs: [
-        {
-            "identifier": "f53b3e0e-6a21-11eb-9439-0242ac130002",
-            "agentIdentifier": "356b03dc-9ec5-11e7-97a6-d501104f897e",
-            "number": "+49151484522",
-            "dateTime": "2020-10-05T14:48:00.000Z",
-            "duration": 230
+      {
+        identifier: '0b96031e-6a22-11eb-9439-0242ac130002',
+        agentIdentifier: '356b03dc-9ec5-11e7-97a6-d501104f897e',
+        number: '+49151484522',
+        dateTime: '2020-10-06T13:50:00.000Z',
+        duration: 93,
+        agent: {
+          identifier: '356b03dc-9ec5-11e7-97a6-d501104f897e',
+          firstName: 'Abraham',
+          lastName: 'Ellis',
+          email: 'Abraham.Ellis@callcenter.xyz',
+          photo: 'https://via.placeholder.com/300/E241BC/FFFFFF?text=Rose.Patterson'
         }
+      },
     ]
 };
 
 describe('PhoneLogs', () => {
     test('updateOrCreatePhoneLog', () => {
         let phoneLogsMap = new Map();
-        const newPhoneLog = updateOrCreatePhoneLog(phoneLogsMap, {
-            "identifier": "f53b3e0e-6a21-11eb-9439-0242ac130002",
-            "agentIdentifier": "356b03dc-9ec5-11e7-97a6-d501104f897e",
-            "number": "+49151484522",
-            "dateTime": "2020-10-05T14:48:00.000Z",
-            "duration": 230
+        let agentsMap = new Map();
+        agentsMap.set('356b03dc-9ec5-11e7-97a6-d501104f897e', {
+            identifier: '356b03dc-9ec5-11e7-97a6-d501104f897e',
+            firstName: 'Abraham',
+            lastName: 'Ellis',
+            email: 'Abraham.Ellis@callcenter.xyz',
+            photo: 'https://via.placeholder.com/300/E241BC/FFFFFF?text=Rose.Patterson'
+        });
+        const newPhoneLog = updateOrCreatePhoneLog(agentsMap, phoneLogsMap, {
+            identifier: '0b96031e-6a22-11eb-9439-0242ac130002',
+            agentIdentifier: '356b03dc-9ec5-11e7-97a6-d501104f897e',
+            number: '+49151484522',
+            dateTime: '2020-10-06T13:50:00.000Z',
+            duration: 93,
         });
         expect(newPhoneLog).toStrictEqual(newPhoneLogDummyData);
     });
