7be66e89dd2f8e083f151dec81b6ac71df89f15d
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2021-12-26 13:12:09 +0300

    add phone logs in main page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,47 @@
 <template>
-  <div class="">Hello Client</div>
+  <div class="phone-logs-content">
+    <div class="">Hello Client</div>
+    <phone-log-list v-if="phoneLogList.length > 0" :phoneLogs="phoneLogList"></phone-log-list>
+  </div>
 </template>
 
 <script>
+import PhoneLogs from '@/components/PhoneLogs.vue';
 
 export default {
   name: 'App',
-  components: {},
+  data() {
+    return {
+      phoneLogs: [],
+    };
+  },
+  computed: {
+    phoneLogList: {
+      get() {
+        return this.phoneLogs;
+      },
+      set(value) {
+        this.phoneLogs = value;
+      },
+    },
+  },
+  methods: {
+    loadLogs() {
+      const self = this;
+      console.log('env ', process.env.VUE_APP_API_URL);
+      fetch(`${process.env.VUE_APP_API_URL}logs`)
+        .then((response) => response.json())
+        .then(({ phoneLogs }) => {
+          self.phoneLogList = phoneLogs;
+        });
+    },
+  },
+  created() {
+    this.loadLogs();
+  },
+  components: {
+    'phone-log-list': PhoneLogs,
+  },
 };
 </script>
 
