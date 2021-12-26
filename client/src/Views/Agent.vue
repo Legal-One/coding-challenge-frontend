5e5580872f5c30e7c12302a2acacf6f428cad359
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2021-12-26 20:08:27 +0300

    add agent view

---
+++ client/src/Views/Agent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="phone-logs-content">
+    <div class="">Agent Details</div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Agent',
+  data() {
+    return {
+      agentDetails: {},
+      agentLogs: [],
+    };
+  },
+  computed: {
+    agent: {
+      get() {
+        return this.agentDetails;
+      },
+      set(value) {
+        this.agentDetails = value;
+      },
+    },
+  },
+  methods: {
+  },
+  components: {
+
+  },
+};
+</script>
+
+<style>
+
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHashHistory } from 'vue-router';
 import App from './App.vue';
+import Agent from '@/Views/Agent.vue';
 
 const routes = [
   { path: '/', component: App },
+  { path: '/agent/:id', component: Agent },
 ];
 
 const router = createRouter({
