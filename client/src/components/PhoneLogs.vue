afafd1392127928add475281552656ab7fecb3d4
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2021-12-26 11:54:14 +0300

    add PhoneLogs

---
+++ client/src/components/PhoneLogs.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="phone-logs">
+    <phone-log-item
+      v-for="(phoneLog, index) in phoneLogs"
+      :phoneLog="phoneLog"
+      :key="index">
+    </phone-log-item>
+  </div>
+</template>
+
+<script>
+import PhoneLogItem from './PhoneLogItem.vue';
+
+export default {
+  name: 'PhoneLogs',
+  props: {
+    phoneLogs: Array,
+  },
+  components: {
+    'phone-log-item': PhoneLogItem,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
