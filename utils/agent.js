ccc535928e48a82d71250f6ba248e698db2a8752
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2021-12-26 16:30:28 +0300

    add getAgentById

--- utils/agent.js
+++ utils/agent.js
@@ -7,7 +7,7 @@ const Agent = {
                 resolve(new Map()); 
                 return;
             }
-            
+
             const agents = JSON.parse(data);
             const agentsMap = new Map();
             agents.forEach((value) => {
@@ -15,6 +15,23 @@ const Agent = {
                 agentsMap.set(identifier, value);
             })
             resolve(agentsMap);
+        });
+    }),
+    getAgentById: (agentIdentifier, path = 'json-data/agents.json') => new Promise((resolve, reject) => {
+        fs.readFile(path, (err, data) => {
+            if (err) { 
+                reject(err); 
+                return;
+            }
+            
+            const agents = JSON.parse(data);
+            const agent = agents.find(({ identifier }) => agentIdentifier === identifier);
+            if (!agent) {
+                reject({ message: 'Agent could not be found' });
+                return;
+            }
+
+            resolve(agent);
         });
     }),
 };
--- utils/test/agent.spec.js
+++ utils/test/agent.spec.js
@@ -1,5 +1,6 @@
 const { 
-    getAgentsMap
+    getAgentsMap,
+    getAgentById
 } = require('../agent');
 const fs = require('fs');
 
@@ -15,4 +16,24 @@ describe('Agent', function() {
             expect(agentsMap.size).toEqual(0);
         });
     });
+
+
+    test('getAgentsById', function() {
+        return getAgentById('356b03dc-9ec5-11e7-97a6-d501104f897e').then((agent) => {
+            expect(agent.identifier).toEqual('356b03dc-9ec5-11e7-97a6-d501104f897e');
+        });
+    });
+
+    test('getAgentsById with wrong path', function() {
+        return getAgentById('356b03dc-9ec5-11e7-97a6-d501104f897e', 'json-data/agents2.json').catch(({ message }) => {
+            expect(message).toEqual(`ENOENT: no such file or directory, open 'json-data/agents2.json'`);
+        });
+    });
+
+    test('getAgentsById with wrong id', function() {
+        return getAgentById('356b03dc-9ec5-11e7-97a6-d501104f897e233').catch(({ message }) => {
+            expect(message).toEqual(`Agent could not be found`);
+        });
+    });
+
 });
