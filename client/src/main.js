570533c675b9df37551070b884146b338d8d34b5
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2021-12-26 13:11:21 +0300

    add vue router

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const routes = [
+  { path: '/', component: App },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+const app = createApp({});
+
+app.use(router);
+
+app.mount('#app');
