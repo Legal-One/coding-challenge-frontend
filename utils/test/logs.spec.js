eba03baf6a907b85f8d6c5e8b49d3cd1c23db1e8
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2021-12-26 21:50:58 +0300

    add spec to logs

--- utils/test/logs.spec.js
+++ utils/test/logs.spec.js
@@ -1,6 +1,7 @@
 const { 
     getResolutionsMap,
-    getPhoneLogsByAgent
+    getPhoneLogsByAgent,
+    getPhoneLogsByNumber
 } = require('../logs'); 
 
 describe('Logs', () => {
@@ -17,8 +18,30 @@ describe('Logs', () => {
     });
 
     test('getPhoneLogsByAgent', () => {
-        return getPhoneLogsByAgent().then((phonelogs) => {
+        return getPhoneLogsByAgent('356b03dc-9ec5-11e7-97a6-d501104f897e').then((phonelogs) => {
+            const filteredLogs = phonelogs.filter(({ agentIdentifier }) => agentIdentifier === '356b03dc-9ec5-11e7-97a6-d501104f897e');
+            const filteredResolutions = phonelogs.filter((item) => item.hasOwnProperty('resolution'));
+            expect(phonelogs.length).toEqual(filteredLogs.length);
+            expect(phonelogs.length).toEqual(filteredResolutions.length);
+        });
+    });
 
+    test('getPhoneLogsByAgent with wrong path', () => {
+        return getPhoneLogsByAgent('356b03dc-9ec5-11e7-97a6-d501104f897e', 'json-data/logs2.json').catch(({ message }) => {
+            expect(message).toEqual(`ENOENT: no such file or directory, open 'json-data/logs2.json'`);
+        });
+    });
+
+    test('getPhoneLogsByNumber', () => {
+        return getPhoneLogsByNumber('+49151484522').then((phonelogs) => {
+            const filteredLogs = phonelogs.filter(({ number }) => number === '+49151484522');
+            expect(phonelogs.length).toEqual(filteredLogs.length);
+        });
+    });
+
+    test('getPhoneLogsByNumber with wrong path', () => {
+        return getPhoneLogsByNumber('+49151484522', new Map(), 'json-data/logs2.json').catch(({ message }) => {
+            expect(message).toEqual(`ENOENT: no such file or directory, open 'json-data/logs2.json'`);
         });
     });
 });
