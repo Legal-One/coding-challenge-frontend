310699b9373110121b052752cb312d18ccba472c
Vikas <vikasvisking007@gmail.com>
2021-12-29 12:15:39 +0530

    setup vue router

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,9 +4,7 @@
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 <template>
-  <h1 class="text-4xl font-bold underline">
-    Hello world!
-  </h1>
+  <router-view/>
 </template>
 
 <style>
