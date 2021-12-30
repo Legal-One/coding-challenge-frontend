bc28620f3e0f3edce1918d40d4170f5434f6f331
Vikas <vikasvisking007@gmail.com>
2021-12-30 16:54:50 +0530

    implement phone number

--- server/index.js
+++ server/index.js
@@ -74,6 +74,28 @@ server.get('/agent-logs/:id', (req, res) => {
   return res.jsonp(parseAgentCallLogs)
 })
 
+server.get('/phone-no-logs/:id', (req, res) => {
+  const phoneNo = req.params?.id
+  const callLogs = db.logs
+  const resolution = db.resolution
+  const agents  = db.agents
+  const phNoLogs = callLogs.filter((callLog) => callLog.number === phoneNo)
+  const phNoResolutionArray = phNoLogs.map((call) => {
+    const callResolution = resolution.find((res) => res.identifier === call.identifier);
+    const agent = agents.find((agent) => agent.identifier === call.agentIdentifier);
+
+    return {
+      phoneNumber: call.number,
+      identifier: call.identifier,
+      callTime: call.dateTime,
+      resolution: callResolution.resolution,
+      agentName: agentFullName(agent),
+      agentIdentifier: agent.identifier
+    };
+  })
+  return res.jsonp(phNoResolutionArray)
+})
+
 server.use(router)
 server.listen(4000, function () {
   console.log(`JSON Server is running ${4000}`)
