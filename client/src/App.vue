33fbe191a23408546afba5f7486b29181f7dce6a
Vikas <vikasvisking007@gmail.com>
2021-12-30 15:54:02 +0530

    minor updates

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,20 @@
 
 <template>
-    <div class="sticky top-0 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-      <h2 class="text-4xl font-bold py-4 text-white"> Call Center Report </h2>
-    </div>
-    <div class="container mx-auto">
-      <router-view/>
-    </div>
+  <div
+    class="
+      sticky
+      top-0
+      bg-gradient-to-r
+      from-indigo-500
+      via-purple-500
+      to-pink-500
+    "
+  >
+    <h2 class="text-4xl font-bold py-4 text-white">Call Center Report</h2>
+  </div>
+  <div class="container mx-auto">
+    <router-view />
+  </div>
 </template>
 
 <style>
